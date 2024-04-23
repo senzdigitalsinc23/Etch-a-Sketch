0ecd0b48d9fda8df40d6af3e34b8dc5c0e611d2c
Douglas Senzu <senzdigitalsinc23@gmail.com>
2024-04-23 23:40:33 +0000

    Added 1000 Vertical Grid Using javascript
    Created 1000 square div boxes using a for loop forming a vertical
    grid layout

---
+++ rsc/js/scripts.js
@@ -0,0 +1,24 @@
+
+let containerDiv = document.querySelector('.container');
+let controlDiv = document.createElement("div");
+
+controlDiv.setAttribute('style', "width: fit-content;")
+
+containerDiv.setAttribute('style', "width: 100%; height: 100%");
+
+let squareGrid = "";
+
+for (let i = 1; i <= 1000; i++) {
+    
+    let gridName = "grid";    
+    squareGrid = document.createElement('div');
+    
+    squareGrid.classList.add(gridName + '-' + i);
+    
+    squareGrid.setAttribute('style', "width: 5px; height: 5px; border: 1px solid black; border-bottom:0;");
+
+    controlDiv.appendChild(squareGrid);
+}
+
+containerDiv.appendChild(controlDiv);
+

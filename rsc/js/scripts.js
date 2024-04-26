330abf6c89b8afb87ceb95fe35d475609ddc1aba
Douglas Senzu <senzdigitalsinc23@gmail.com>
2024-04-26 02:36:57 +0000

    Massive changes
    Add most of the functionalities to the app including color on hovering,
    opacity in grids when moving over. Project about 90% complete

--- rsc/js/scripts.js
+++ rsc/js/scripts.js
@@ -1,8 +1,22 @@
 
 let containerMain = document.querySelector('.container');
 
-function createElements(element) {
-    return document.createElement(element);
+let gridContainer = createElements("div");
+let itemsContainer = createElements("div");
+let buttonsContainer = createElements("div");
+let btnGridSize = createElements("Button", "Set Grid Size");
+let btnReset = createElements("Button", "Clear Screen");
+let row = "";
+let square = "";
+
+//Helper functions for creating and setting elements on DOM
+function createElements(element, text = "") {
+
+    let create = document.createElement(element)
+
+    create.textContent = text;
+
+    return create;
 }
 
 
@@ -26,13 +40,122 @@ function attachElements(parent, child) {
     parent.append(child);
 }
 
-let container = createElements("div");
+//Container for items
+itemsContainer.classList.add('items-container');
 
+attachElements(containerMain, itemsContainer);
+
+//Container for buttons
 setAttributes({
-    'color': "red",
-    'border': "2px solid orange",
-    'width': "500px",
-    'height': "500px"
-}, container);
+    'display': "flex",
+    'justify-content': "center",
+    'padding': "20px",
+    'gap': "20px"
+}, buttonsContainer);
 
-attachElements(containerMain, container)
+attachElements(containerMain, buttonsContainer);
+
+//Button for setting grid size
+btnGridSize.classList.add("btn-grid-size");
+
+attachElements(buttonsContainer, btnGridSize)
+
+
+//Button for resetting grid 
+btnReset.classList.add("btn-reset");
+
+attachElements(buttonsContainer, btnReset)
+
+
+//Grid container for grids
+
+gridContainer.classList.add('grid-container')
+
+attachElements(containerMain, gridContainer)
+
+
+//Print grid
+function printGrid() {
+    
+    let n = gridSize; // row or column count
+
+    console.log(gridSize)
+    // defining an empty string
+    let string = "";
+
+    for (let i = 1; i <= n; i++) { // external loop
+        
+        row = createElements('div');
+        let rowSize = 500 / gridSize;
+
+        setAttributes({
+            'height': rowSize + "px"
+        }, row);
+
+        attachElements(gridContainer, row);
+
+        row.classList.add('row');
+        
+        for (let j = 1; j <= n; j++) {
+
+            square = createElements("div");
+            let squareSize = 500 / gridSize;
+
+            attachElements(row, square);
+
+            setAttributes({
+                'width': squareSize + "px",
+                'height': squareSize + "px",
+                'padding': 0,
+                'margin': 0
+            }, square);
+
+            square.classList.add("square");
+
+
+            let opacity = 0.2;
+
+            square.addEventListener('mouseover', (e) => {
+                if (opacity < 1) {
+                    opacity += 0.2;                    
+                    e.target.style = "background-color: red; opacity:" + opacity.toString();
+
+                }               
+            })
+        }
+        
+    }
+    
+}
+
+
+function clearGrid() {
+    gridContainer.textContent = "";
+
+    printGrid();
+}
+
+
+
+btnReset.onclick = () => {
+    clearGrid();
+}
+
+//Getting grid size from user
+let gridSize = 16;
+
+btnGridSize.onclick = () => {    
+    gridSize = prompt("Enter number squares per grid. (NB: Max is 100)"); 
+    
+    if (gridSize < 5 || gridSize > 100) {
+        alert("Enter a valid figure (Between 5 and 100)")
+    } else {
+        
+
+        clearGrid();
+        
+       
+    }
+}
+
+printGrid();
